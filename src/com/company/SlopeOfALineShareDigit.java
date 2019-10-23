6bdf069fdfa5d412611d51830ef00c417fd1d1d6
lish <shuzhou.li@ssfs.org>
2019-10-23 14:39:37 -0400

    yt

--- src/com/company/SlopeOfALineShareDigit.java
+++ src/com/company/SlopeOfALineShareDigit.java
@@ -12,6 +12,11 @@ public class SlopeOfALineShareDigit {
         System.out.println("Enter the y coordinate of the second point: ");
         int y2 = input.nextInt();
         System.out.println(slope(x1, y1, x2, y2));
+
+        System.out.println();
+        System.out.println("this is the share digit method:");
+        //you said you don't want input from the user
+        System.out.println(ShareDigits(34,22));
     }
     private static String slope(int x1, int y1, int x2, int y2) {
         if (x1 == x2) {
@@ -19,6 +24,23 @@ public class SlopeOfALineShareDigit {
         } else {
             double slope = (y2-y1)/(x2-x1);
             return "The line defined by the points (" + x1 + ", " + y1 + ") and (" + x2 + ", " + y2 + ") is not a vertical line and the slope is "+slope+".";
+        }
+    }
+    private static boolean ShareDigits(int a, int b){
+        int aten = a/10;
+        int aone = a%10;
+        int bten = b/10;
+        int bone = b%10;
+        if(aten==bten){
+            return true;
+        }else if(aten==bone){
+            return true;
+        }else if(aone == bten){
+            return true;
+        }else if(aone==bone){
+            return true;
+        }else{
+            return false;
         }
     }
 }
