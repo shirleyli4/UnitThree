91d9ea109a7012a2cfe2b46a4f1a154d4f246875
lish <shuzhou.li@ssfs.org>
2019-10-29 11:21:04 -0400

    yt

--- src/com/company/RightTriangle.java
+++ src/com/company/RightTriangle.java
@@ -1,7 +1,19 @@
+//not finished
 package com.company;
-
+import java.util.Scanner;
 public class RightTriangle {
     public static void main(String[] args){
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter three integer sides of a triangle:");
+    int a = input.nextInt();
+    int b = input.nextInt();
+    int c = input.nextInt();
+    System.out.println(isRight(isTriangle(a,b,c)));
+    }
+    public static String isRight(int a,int b,int c){
+
+    }
+    public static String isTriangle(int a,int b,int c){
 
     }
 }
