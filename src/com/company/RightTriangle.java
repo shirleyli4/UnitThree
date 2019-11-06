1031abafa3d5ae1a0692c350dea2847b357daf0e
lish <shuzhou.li@ssfs.org>
2019-11-06 08:56:10 -0500

    yt

--- src/com/company/RightTriangle.java
+++ src/com/company/RightTriangle.java
@@ -8,12 +8,28 @@ public class RightTriangle {
     int a = input.nextInt();
     int b = input.nextInt();
     int c = input.nextInt();
-    System.out.println(isRight(isTriangle(a,b,c)));
-    }
-    public static String isRight(int a,int b,int c){
 
+    System.out.println(isTriangle(a,b,c));
+    System.out.println(isRight(a,b,c));
     }
-    public static String isTriangle(int a,int b,int c){
+    public static boolean isRight(int a,int b,int c){
+        if(Math.pow(a,2)+Math.pow(b,2)==Math.pow(c,2)||Math.pow(a,2)+Math.pow(c,2)==Math.pow(b,2)||Math.pow(c,2)+Math.pow(b,2)==Math.pow(a,2)){
+            return true;
+        }else{
+            return false;
+        }
+    }
 
+
+    public static boolean isTriangle(int a,int b,int c){
+        if(a+b>c&&a+c>b&&b+c>a){
+                return true;
+                System.out.println("The side lengths of " + a + ", " + b + ", and " + c + " form a right triangle, but it is not a right triangle");
+            }else{
+                return false;
+                System.out.println("The side lengths of " + a + ", " + b + ", and " + c + " form a triangle, but it is not a right triangle");
+            }
+        }
     }
-}
+
+
