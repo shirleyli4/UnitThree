90144347e8be384a2a7146db8b4fc28522b0cbc9
lish <shuzhou.li@ssfs.org>
2019-11-07 10:07:46 -0500

    yt

--- src/com/company/RightTriangle.java
+++ src/com/company/RightTriangle.java
@@ -1,4 +1,4 @@
-//not finished
+
 package com.company;
 import java.util.Scanner;
 public class RightTriangle {
@@ -8,9 +8,14 @@ public class RightTriangle {
     int a = input.nextInt();
     int b = input.nextInt();
     int c = input.nextInt();
+        if (isTriangle(a, b, c) && isRight(a, b, c)) {
+            System.out.println("The side lengths of " + a + ", " + b + ", and " + c + " form a right triangle");
+        }else if(isTriangle(a, b, c) && !isRight(a, b, c)){
+            System.out.println("The side lengths of " + a + ", " + b + ", and " + c + " form a triangle, but it is not a right triangle");
+        }else{
+            System.out.println("The side lengths of " + a + ", " + b + ", and " + c + " do not form a triangle");
+        }
 
-    System.out.println(isTriangle(a,b,c));
-    System.out.println(isRight(a,b,c));
     }
     public static boolean isRight(int a,int b,int c){
         if(Math.pow(a,2)+Math.pow(b,2)==Math.pow(c,2)||Math.pow(a,2)+Math.pow(c,2)==Math.pow(b,2)||Math.pow(c,2)+Math.pow(b,2)==Math.pow(a,2)){
@@ -24,10 +29,8 @@ public class RightTriangle {
     public static boolean isTriangle(int a,int b,int c){
         if(a+b>c&&a+c>b&&b+c>a){
                 return true;
-                System.out.println("The side lengths of " + a + ", " + b + ", and " + c + " form a right triangle, but it is not a right triangle");
             }else{
                 return false;
-                System.out.println("The side lengths of " + a + ", " + b + ", and " + c + " form a triangle, but it is not a right triangle");
             }
         }
     }
