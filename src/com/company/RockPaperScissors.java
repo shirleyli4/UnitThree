fed6360c12832036e0c54f1074b7f324c7ce2fec
lish <shuzhou.li@ssfs.org>
2019-11-07 10:15:58 -0500

    yt

---
+++ src/com/company/RockPaperScissors.java
@@ -0,0 +1,8 @@
+//not finish
+package com.company;
+import java.util.Scanner;
+public class RockPaperScissors {
+    public static void main(String[] args){
+        System.out.println("Please choose either rock, paper, or scissors.");
+    }
+}
