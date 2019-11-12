5336b00fa4d8262272c7f192facf01c61f48ff33
lish <shuzhou.li@ssfs.org>
2019-11-12 13:41:33 -0500

    yt

--- src/com/company/RockPaperScissors.java
+++ src/com/company/RockPaperScissors.java
@@ -5,4 +5,16 @@ public class RockPaperScissors {
     public static void main(String[] args){
         System.out.println("Please choose either rock, paper, or scissors.");
     }
+    public static String getUserChoice(){
+
+    }
+    public static String getComputerChoice(){
+
+    }
+    public static String whoWins(String computer, String person){
+
+    }
+
+
+
 }
